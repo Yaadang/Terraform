b707ea51d6649b75ae9e967258d3fc8e5486e98a
Yaadang <devang_lamba@hotmail.com>
2023-06-20 17:27:49 -0700

    added sg's and vpcs

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
-
+variable myip {}
 
 resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
@@ -28,8 +28,8 @@ resource "aws_internet_gateway" "myapp-igw" {
         Name: " ${var.env_prefix}-igw"
     }
 }
-
-resource "aws_route_table" "myapp-route-table"{
+//If we choose not to use the default RTB and create our own RTB
+/*resource "aws_route_table" "myapp-route-table"{
     vpc_id = aws_vpc.myapp-vpc.id
     route {
         cidr_block = "0.0.0.0/0"
@@ -38,5 +38,51 @@ resource "aws_route_table" "myapp-route-table"{
     tags = {
         Name : " ${var.env_prefix}-routeTable"
     }
+}*/
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet.id
+    route_table_id = aws_default_route_table.main-rtb.id
 }
 
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name : " ${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_default_security_group" "myapp-sg"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.myip]
+    }
+    ingress{
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-SG"
+    }
+
+}
