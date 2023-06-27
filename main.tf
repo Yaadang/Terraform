57649c2a38b91a8d352e52b8e5067975d65e9c89
Yaadang <107151903+Yaadang@users.noreply.github.com>
2023-06-27 15:51:57 -0700

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,48 +1,37 @@
-provider "aws" {}
-
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable myip {}
-
-resource "aws_vpc" "myapp-vpc"{
-    cidr_block = var.vpc_cidr_block
-    tags = {
-        Name = "${var.env_prefix}-vpc"
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "myapp-bucket"
+        key = "myapp/state.tfstate"
+        region = "eu-west-3"
     }
 }
 
-resource "aws_subnet" "myapp-subnet"{
+provider "aws" {
+    region = var.region
+}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
     vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-        Name = " ${var.env_prefix}-subnet1"
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
     tags = {
-        Name: " ${var.env_prefix}-igw"
+        Name: "${var.env_prefix}-igw"
     }
-}
-//If we choose not to use the default RTB and create our own RTB
-/*resource "aws_route_table" "myapp-route-table"{
-    vpc_id = aws_vpc.myapp-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name : " ${var.env_prefix}-routeTable"
-    }
-}*/
-
-resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet.id
-    route_table_id = aws_default_route_table.main-rtb.id
 }
 
 resource "aws_default_route_table" "main-rtb" {
@@ -53,20 +42,21 @@ resource "aws_default_route_table" "main-rtb" {
         gateway_id = aws_internet_gateway.myapp-igw.id
     }
     tags = {
-        Name : " ${var.env_prefix}-main-rtb"
+        Name: "${var.env_prefix}-main-rtb"
     }
 }
 
-resource "aws_default_security_group" "myapp-sg"{
+resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.myapp-vpc.id
-    
-    ingress{
+
+    ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = [var.myip]
+        cidr_blocks = [var.my_ip, var.jenkins_ip]
     }
-    ingress{
+
+    ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
@@ -82,7 +72,41 @@ resource "aws_default_security_group" "myapp-sg"{
     }
 
     tags = {
-        Name: "${var.env_prefix}-SG"
+        Name: "${var.env_prefix}-default-sg"
     }
+}
 
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "myapp-key-pair"
+
+    user_data = file("entry-script.sh")
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
 }
